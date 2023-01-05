9751810b8db9ad84600c1f81b28b219fcb6c4582
Adrian Lawson <adrianglawson@gmail.com>
2023-01-04 16:47:04 -0800

    mint nft with no gas

--- contracts/RecipientExample.sol
+++ contracts/RecipientExample.sol
@@ -1,107 +1,21 @@
 // SPDX-License-Identifier: No License
 pragma solidity 0.8.17;
-
 import "./Ownable.sol";
+import "./ERC721A.sol";
 
-interface IERC20 {
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns (uint256);
-
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `to`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transfer(address to, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Moves `amount` tokens from `from` to `to` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-}
-
-contract RecipientExample is Ownable {
-    IERC20 public token;
-
-    event paid(address payer, uint256 amount, uint256 item);
-    function pay(uint256 amount, uint256 item) external {
-        address caller = msgSender();
-        bool success = token.transferFrom(caller, address(this), amount);
-        require(success, 'Transfer fail');
-        emit paid(caller, amount, item);
-    }
-
-    function withdrawtoken() external onlyOwner {
-        bool success = token.transfer(owner(), token.balanceOf(address(this)));
-        require(success, 'Transfer fail');
-    }
-
-    function tokenBal() external view returns(uint256){
-        return token.balanceOf(address(this));
-    }
-
+contract GaslessNFT is Ownable, ERC721A {
     address immutable _trustedForwarder;
-    constructor(address trustedForwarder, address _token) {
+
+    event minted(address minter, uint256 amount);
+    constructor(address trustedForwarder) ERC721A("Gasless NFT","GNFT") {
         _trustedForwarder = trustedForwarder;
-        token = IERC20(_token);
+    }
+
+    function mint(uint256 amount) external {
+        address sender = msgSender();
+
+        _safeMint(sender, amount);
+        emit minted(sender, amount);
     }
 
     function isTrustedForwarder(address forwarder) public view returns(bool) {
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -14,18 +14,18 @@ let abiCoder;
 let link = etherscan;
 
 const relayer = '0x2A0d1f0EE9c5584b1694BCa16879423432770A52';
-const recipient = '0x62aEeD88eA286283D86ac1a00164073028bF3689';
-const token = '0x24c8809834Ee44419168B4ceE26BBa5188595891';
+const recipient = '0x85Ee6Ce038A5518Eb7897578ffDf675eF06dB3F7' //'0x62aEeD88eA286283D86ac1a00164073028bF3689';
 
 function App() {
 
-  async function getSigner () {
+  async function getSigner (e) {
+    e.preventDefault();
     abiCoder = new ethers.utils.AbiCoder();
     try{
       provider = new ethers.providers.Web3Provider(window.ethereum)
       forwarder = new ethers.Contract(relayer, MFABI, provider);
       await provider.send("eth_requestAccounts", []);
-      token_contract = new ethers.Contract(token, TOKEN_ABI, provider);
+      //token_contract = new ethers.Contract(token, TOKEN_ABI, provider);
       signer = await provider.getSigner();
       userAddress = await signer.getAddress();
 
@@ -39,21 +39,16 @@ function App() {
         });
        }
 
-      const allowance = await token_contract.allowance(userAddress, recipient);
-      if(parseInt(allowance) < 1000) {
-        setMessage('Approve Token Spend');
-      } else {
-        setMessage('Sign Message');
-      }
+      // const allowance = await token_contract.allowance(userAddress, recipient);
+      // if(parseInt(allowance) < 1000) {
+      //   setMessage('Approve Token Spend');
+      // } else {
+      setMessage('Mint');
+      // }
   
     } catch{
       getSigner();
     }
-  }
-
-  async function mint() {
-    await getSigner();
-    await token_contract.connect(signer).mint('100000000000000000000');
   }
 
   async function bal() {
@@ -61,20 +56,21 @@ function App() {
     return parseInt(balance);
   }
 
-  async function signMessage () {
+  async function signMessage (e) {
+    e.preventDefault();
     const nonce = await forwarder.getNonce(userAddress);
-    const allowance = await token_contract.allowance(userAddress, recipient);
-    if(parseInt(allowance) < 1000) {
-      try{
-        const approvaltx = await token_contract.connect(signer).approve(recipient, '1000000000000000000000000');
-        await approvaltx.wait(1);
-        setMessage('Sign Message');
-      } catch (error) {
-        alert(error.message);
-      }
-    }
+    // const allowance = await token_contract.allowance(userAddress, recipient);
+    // if(parseInt(allowance) < 1000) {
+    //   try{
+    //     const approvaltx = await token_contract.connect(signer).approve(recipient, '1000000000000000000000000');
+    //     await approvaltx.wait(1);
+    //     setMessage('Sign Message');
+    //   } catch (error) {
+    //     alert(error.message);
+    //   }
+    // }
 
-    let data = abiCoder.encode(['uint256', 'uint256'], ['1000000000000000000', '0']);
+    let data = abiCoder.encode(['uint256'], [amount]);
     data = data.slice(2,data.length);
     // console.log(data);
     const Req = {
@@ -83,7 +79,7 @@ function App() {
       value: 0,
       gas: 100000,
       nonce: nonce,
-      data: '0xef48eee6' + data
+      data: '0xa0712d68' + data
     }
 
     let message = ethers.utils.solidityKeccak256(
@@ -109,22 +105,39 @@ function App() {
     }
   }
 
+  function updateAmount(direction) {
+    if(userAddress != '') {
+      if(direction == 'up' && amount < 10) {
+        setAmount(amount + 1);
+      } else if (direction == 'down' && amount > 1) {
+        setAmount(amount - 1);
+      }
+    }
+  }
+
+  const [amount, setAmount] = useState(1);
   const [message, setMessage] = useState('Connect Wallet');
 
   return (
     <div className="App">
       <h1>Gasless Tx</h1>
       <p>Goerli Network</p>
-      <p>Approve token spends once and transfer with no gas fees!</p>
+      <p>Mint an NFT with No Gas Fees!</p>
       <div id="etherscanLink">
 
       </div>
       <div className="card">
         <p>{message == 'Connect Wallet' ? 'Not Connected' : 'Connected: ' + userAddress}</p>
-        <button onClick={() => mint()}>Mint 100 Test Tokens</button>
-        <button onClick={() => message == 'Connect Wallet' ? getSigner() : signMessage()}>
+        {/* <button onClick={() => mint()}>Mint 100 Test Tokens</button> */}
+        {/* <button onClick={() => message == 'Connect Wallet' ? getSigner() : signMessage()}>
           {message}
-        </button>
+        </button> */}
+
+        <form onSubmit={e => message == 'Connect Wallet' ? getSigner(e) : signMessage(e)}>
+          <button type="submit">{message == 'Connect Wallet' ? message : message + ' ' + amount}</button>
+			  </form>
+        <button onClick={() => updateAmount('up')}>^</button>
+        <button onClick={() => updateAmount('down')}>v</button>
         
       </div>
     </div>
